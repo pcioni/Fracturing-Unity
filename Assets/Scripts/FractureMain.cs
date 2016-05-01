50a8fee423270850e0ff9cb340d7f0d953f8909b
Philip Cioni <philcioni@gmail.com>
2016-05-01 00:57:50 -0400

    Preliminary script organizing

---
+++ Assets/Scripts/FractureMain.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FractureMain : MonoBehaviour {
+
+	private int generation = 1;
+	private int maxGenerations = 3;
+	private int numCutsToMake = 2;
+
+	private ConvexHull hull;
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	/// Fractures a GameObject at a point, instantiating the pieces as new GameObjects.
+	/// Create local planes -> create new convex hulls -> create new GameObjects
+	public void Fracture(Vector3 point) {
+
+	}
+}
--- Assets/Scripts/Triangulation.cs
+++ Assets/Scripts/Triangulation.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class dummy : MonoBehaviour {
+public class Triangulation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
